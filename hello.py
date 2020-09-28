c13e70c335b135335e966ffff9291dc36f9828df
NikitaMityushov <NikitaMityushov@gmail.com>
2020-09-28 12:21:34 +0000

    master commit #6

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(environ, start_response):
-	start_response('200 OK', [('Content-Type', 'text/html')])
+	start_response('200 OK', [('Content-Type', 'text/plain')])
 	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 	return body
