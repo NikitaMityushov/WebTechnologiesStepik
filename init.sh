99dcdc969e08ce3e7bbcb357d8e5aa49faaa4bec
NikitaMityushov <NikitaMityushov@gmail.com>
2020-09-28 11:25:20 +0000

    master commit 5

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/etc/hello.py  /etc/gunicorn.d/hello.py
+
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
