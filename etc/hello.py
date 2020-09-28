cf1b8ad1338bf2f294a87403dfba795c801e464f
NikitaMityushov <NikitaMityushov@gmail.com>
2020-09-28 19:17:28 +0000

    master commit 6

--- etc/hello.py
+++ etc/hello.py
@@ -1,2 +1,3 @@
 import multiprocessing
+application = '/home/box/web/hello.py'
 bind = "0.0.0.0:8080"
--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/etc/hello.py  /etc/gunicorn.d/hello.py
 
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/nginx restart
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+
