cb7c15027c570789fc6178343aef6e46d980f77c
NikitaMityushov <NikitaMityushov@gmail.com>
2020-09-29 04:55:01 +0000

    master commit 8

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo gunicorn -b 0.0.0.0:8080 hello:application
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
 
