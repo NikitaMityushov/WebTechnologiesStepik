37d9111dd4c801559bf7e46d98a78277bc176fd7
NikitaMityushov <NikitaMityushov@gmail.com>
2020-10-01 10:04:03 +0000

    master commit 13

--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,2 @@
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepic_web;"
-mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
+sudo python3 ~/web/ask/manage.py makemigrations
+sudo python3 ~/web/ask/manage.py migrate
