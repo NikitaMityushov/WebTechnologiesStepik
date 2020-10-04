2f70b1323b5a25cfd6d42dde410b1337dd8cadf3
mityushovn <nikitamityushov@gmail.com>
2020-10-04 21:48:46 +0300

    master commit 18

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by("-id")
+        return self.order_by("-added_at")
     
     def popular(self):
         return self.order_by("-rating") 
@@ -16,7 +16,7 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=256)
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes_set')
@@ -27,6 +27,6 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, Http404
 from django.core.paginator import Paginator, EmptyPage
 from qa.models import Question, Answer
+from django.contrib.auth.models import User
+import datetime
 
 
 # Create your views here.
@@ -11,6 +13,7 @@ def test(request, *args, **kwargs):
 
 
 def display_new_question(request, *args, **kwargs):
+    # fill_db()
     tags = Question.objects.new()
     try:
         limit = int(request.GET.get('limit', 10))
@@ -69,3 +72,19 @@ def display_concrete(request, req_id):
     return render(request, 'con_ques.html', {
         'question': question,
     })
+
+
+def fill_db():
+    user, _ = User.objects.get_or_create(                                                                                                        
+        username='x',                                                                                                                            
+        defaults={'password':'y', 'last_login': datetime.datetime.now()})
+
+    for i in range(0, 30):
+        title = 'question' + str(i)
+        text = 'text' + str(i)
+        author = user
+        rating = 0 + i
+        Question.objects.create(title=title, text=text, author=author, rating=rating)
+
+    Question.objects.create(title='question last', text='text', author=user)  
+    Question.objects.get_or_create(pk=3141592, title='question about pi', text='what is the last digit?', author=user)
