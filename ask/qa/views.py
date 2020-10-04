6e60790be3c0ef38bda5e948849cf3b025602c7f
mityushovn <nikitamityushov@gmail.com>
2020-10-04 18:20:27 +0300

    master commit 16

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, Http404
+from django.core.paginator import Paginator, EmptyPage
 from qa.models import Question, Answer
 
 
@@ -11,24 +11,61 @@ def test(request, *args, **kwargs):
 
 
 def display_new_question(request, *args, **kwargs):
-    question_list = Question.objects.all().order_by("-id")
-    paginator = Paginator(question_list, 10)
+    tags = Question.objects.new()
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 10:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(tags, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
 
-    page = request.GET.get('page')
-    questions = paginator.get_page(page)
-    return render(request, "index.html", {'questions': questions})
+    paginator.baseurl = '?page='
+
+    return render(request, 'display_new.html', {
+        'questions': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
 
 
 def display_popular(request, *args, **kwargs):
-    question_list = Question.objects.popular().order_by("-rating")
-    paginator = Paginator(question_list, 10)
+    tags = Question.objects.popular()
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 10:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(tags, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
 
-    page = request.GET.get('page')
-    questions = paginator.get_page(page)
-    return render(request, "index.html", {'questions': questions})
+    paginator.baseurl = '?page='
+
+    return render(request, 'display_popular.html', {
+        'questions': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
 
 
 def display_concrete(request, req_id):
-    question = Question.objects.filter(id=req_id).first()
-    answers = Answer.objects.filter(question=question)
-    return render(request, "con_ques.html", {'question': question}, {'answers': answers})
+    question = get_object_or_404(Question, id=req_id)
+    return render(request, 'con_ques.html', {
+        'question': question,
+    })
